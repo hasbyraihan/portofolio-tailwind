80425ef21405da677e349b0cb4c75d83bd114d7f
hasbyraihan <hasby608@gmail.com>
2024-05-29 15:42:48 +0700

    Menyempurnakan

--- dist/js/script.js
+++ dist/js/script.js
@@ -112,3 +112,16 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', addAnimation);
     addAnimation();
 });
+
+
+//Scroll Navbar
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
